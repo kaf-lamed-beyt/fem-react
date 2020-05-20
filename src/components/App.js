9d5c157c0eb77a234cf9e0700ce06e2639c31aff
Caleb335 <belac335@gmail.com>
2020-05-20 14:18:56 +0100

    refactoring components

--- src/components/App.js
+++ src/components/App.js
@@ -34,7 +34,6 @@ export default class App extends React.Component {
                         />
                     </label>
                 </form>
-                <PostForm title={currentTitle} body={currnetBody} />
             </div>
         )
     }
