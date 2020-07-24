07dff7911f22ca228099e0110df0343eefb067ad
Caleb335 <belac335@gmail.com>
2020-07-24 15:14:15 +0100

    map function

---
+++ src/components/App.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react'
+
+const list = [
+    {
+        title: 'React',
+        url: 'https://facebook.github.io/react/',
+        author: 'Jordan Walke',
+        num_comments: 3,
+        points: 4,
+        objectID: 0,
+    },
+    {
+        title: 'Redux',
+        url: 'https://github.com/reactjs/redux',
+        author: 'Dan Abramov, Andrew Clark',
+        num_comments: 2,
+        points: 5,
+        objectID: 1,
+    },
+]
+
+export default class App extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            list: list,
+        }
+    }
+
+    render() {
+        const { list } = this.state
+
+        return (
+            <div className="base">
+                <h1>Hey there! 😉 </h1>
+                {/* undirectional data flow */}
+                {
+                    list.map((item) => {
+                        return (
+                            <div key={item.objectID}>
+                                <span>
+                                    <a href={item.url}>{item.title}</a>
+                                </span>
+                            </div>
+                        )
+                    })}
+            </div>
+        )
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,18 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import App from './components/App'
-;<React.Fragment>
-    <Router>
-        <Switch>
-            <Route exact path="/" component={App} />
-        </Switch>
-    </Router>
-</React.Fragment>
 
 ReactDOM.render(<App />, document.getElementById('root'))
-
-// hot module replacement
-if (module.hot) {
-    module.hot.accept()
-}
