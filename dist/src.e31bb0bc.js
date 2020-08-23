8df53692ad4325524224e1ad91a0f3915d41468d
Caleb335 <belac335@gmail.com>
2020-08-23 18:14:49 +0100

    conditional rendering works

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -28285,37 +28285,7 @@ if ("development" === 'production') {
 } else {
   module.exports = require('./cjs/react-dom.development.js');
 }
-},{"./cjs/react-dom.development.js":"../node_modules/react-dom/cjs/react-dom.development.js"}],"components/Table.js":[function(require,module,exports) {
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = Greeting;
-
-var _react = _interopRequireDefault(require("react"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-// conditional rendering of components
-function UserGreeting(props) {
-  return /*#__PURE__*/_react.default.createElement("h1", null, "Welcome back!");
-}
-
-function GuestGreeting(props) {
-  return /*#__PURE__*/_react.default.createElement("h1", null, "Please sign up");
-}
-
-function Greeting(props) {
-  var isLoggedIn = props.isLoggedIn;
-
-  if (isLoggedIn) {
-    return /*#__PURE__*/_react.default.createElement(UserGreeting, null);
-  }
-
-  return /*#__PURE__*/_react.default.createElement(GuestGreeting, null);
-}
-},{"react":"../node_modules/react/index.js"}],"components/LoginControl.js":[function(require,module,exports) {
+},{"./cjs/react-dom.development.js":"../node_modules/react-dom/cjs/react-dom.development.js"}],"components/App.js":[function(require,module,exports) {
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
@@ -28324,8 +28294,6 @@ Object.defineProperty(exports, "__esModule", {
 exports.default = void 0;
 
 var _react = _interopRequireDefault(require("react"));
-
-var _Table = require("./Table");
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
@@ -28351,17 +28319,38 @@ function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Re
 
 function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
 
-function LoginButton(props) {
+var UserGreeting = function UserGreeting(props) {
+  return /*#__PURE__*/_react.default.createElement("h1", null, "Welcome Back!");
+};
+
+var GuestGreeting = function GuestGreeting(props) {
+  return /*#__PURE__*/_react.default.createElement("h1", null, "Please sign up.");
+};
+
+var Greeting = function Greeting(props) {
+  var isLoggedIn = props.isLoggedIn;
+
+  if (isLoggedIn) {
+    return /*#__PURE__*/_react.default.createElement(UserGreeting, null);
+  }
+
+  return /*#__PURE__*/_react.default.createElement(GuestGreeting, null);
+}; // button components
+
+
+var LoginButton = function LoginButton(props) {
   return /*#__PURE__*/_react.default.createElement("button", {
     onClick: props.onClick
   }, "Login");
-}
+};
 
-function LogoutButton(props) {
+var LogoutButton = function LogoutButton(props) {
   return /*#__PURE__*/_react.default.createElement("button", {
     onClick: props.onClick
   }, "Logout");
-}
+}; // stateful class component
+// to capture button's click state
+
 
 var LoginControl = /*#__PURE__*/function (_React$Component) {
   _inherits(LoginControl, _React$Component);
@@ -28375,23 +28364,25 @@ var LoginControl = /*#__PURE__*/function (_React$Component) {
 
     _this = _super.call(this, props);
     _this.state = {
-      isLoggedIn: false
+      isLoggedIn: true
     };
-    _this.onLoginClick = _this.onLoginClick.bind(_assertThisInitialized(_this));
-    _this.onLogoutClick = _this.onLogoutClick.bind(_assertThisInitialized(_this));
+    _this.onLogin = _this.onLogin.bind(_assertThisInitialized(_this));
+    _this.onLogout = _this.onLogout.bind(_assertThisInitialized(_this));
     return _this;
   }
 
   _createClass(LoginControl, [{
-    key: "onLoginClick",
-    value: function onLoginClick() {
+    key: "onLogin",
+    value: function onLogin() {
+      console.log("you're logged in");
       this.setState({
         isLoggedIn: true
       });
     }
   }, {
-    key: "onLogoutClick",
-    value: function onLogoutClick() {
+    key: "onLogout",
+    value: function onLogout() {
+      console.log("you're logged out");
       this.setState({
         isLoggedIn: false
       });
@@ -28399,25 +28390,23 @@ var LoginControl = /*#__PURE__*/function (_React$Component) {
   }, {
     key: "render",
     value: function render() {
-      // destructuring state variable
-      var isLoggedIn = this.state.isLoggedIn; // storing the event handlersin variable names
-      // so it'd be easey for me to call.
-
-      var onLogin = this.onLoginClick;
-      var onLogout = this.onLogoutClick;
-      var button; // conditional rendering logic
+      //  destructuring component state assignment
+      var isLoggedIn = this.state.isLoggedIn;
+      var button; // shortening constructor methods
+      // const login = this.onLogin()
+      // const logout = this.onLogout()
 
       if (isLoggedIn) {
-        button = /*#__PURE__*/_react.default.createElement(LogoutButton, {
-          onClick: onLogout
+        button = /*#__PURE__*/_react.default.createElement(LoginButton, {
+          onClick: this.onLogin
         });
       } else {
-        button = /*#__PURE__*/_react.default.createElement(LoginButton, {
-          onClick: onLogin
+        button = /*#__PURE__*/_react.default.createElement(LogoutButton, {
+          onClick: this.onLogout
         });
       }
 
-      return /*#__PURE__*/_react.default.createElement("div", null, /*#__PURE__*/_react.default.createElement(_Table.Greeting, {
+      return /*#__PURE__*/_react.default.createElement("div", null, /*#__PURE__*/_react.default.createElement(Greeting, {
         isLoggedIn: isLoggedIn
       }), button);
     }
@@ -28426,70 +28415,13 @@ var LoginControl = /*#__PURE__*/function (_React$Component) {
   return LoginControl;
 }(_react.default.Component);
 
-exports.default = LoginControl;
-},{"react":"../node_modules/react/index.js","./Table":"components/Table.js"}],"components/App.js":[function(require,module,exports) {
-"use strict";
+var App = function App() {
+  return /*#__PURE__*/_react.default.createElement("div", null, /*#__PURE__*/_react.default.createElement("h1", null, "Hey there"), /*#__PURE__*/_react.default.createElement(LoginControl, null));
+};
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = void 0;
-
-var _react = _interopRequireDefault(require("react"));
-
-var _Table = _interopRequireDefault(require("./Table"));
-
-var _LoginControl = _interopRequireDefault(require("./LoginControl"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
-
-function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
-
-function _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function _createSuperInternal() { var Super = _getPrototypeOf(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
-
-function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
-
-function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
-
-function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Date.prototype.toString.call(Reflect.construct(Date, [], function () {})); return true; } catch (e) { return false; } }
-
-function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
-
-var App = /*#__PURE__*/function (_React$Component) {
-  _inherits(App, _React$Component);
-
-  var _super = _createSuper(App);
-
-  function App() {
-    _classCallCheck(this, App);
-
-    return _super.apply(this, arguments);
-  }
-
-  _createClass(App, [{
-    key: "render",
-    value: function render() {
-      return /*#__PURE__*/_react.default.createElement("div", null, /*#__PURE__*/_react.default.createElement(_Table.default, {
-        isLoggedIn: false
-      }), /*#__PURE__*/_react.default.createElement(_LoginControl.default, null));
-    }
-  }]);
-
-  return App;
-}(_react.default.Component);
-
-exports.default = App;
-},{"react":"../node_modules/react/index.js","./Table":"components/Table.js","./LoginControl":"components/LoginControl.js"}],"index.js":[function(require,module,exports) {
+var _default = App;
+exports.default = _default;
+},{"react":"../node_modules/react/index.js"}],"index.js":[function(require,module,exports) {
 "use strict";
 
 var _react = _interopRequireDefault(require("react"));
@@ -28529,7 +28461,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "37081" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "43877" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,108 @@
 import React from 'react'
-import Table from './Table'
-import LoginControl from './LoginControl'
 
 
-export default class App extends React.Component {
+const UserGreeting = (props) => {
+  return (
+    <h1>Welcome Back!</h1>
+  )
+}
+
+const GuestGreeting = (props) => {
+  return (
+    <h1>Please sign up.</h1>
+  )
+}
+
+
+const Greeting = (props) => {
+  const isLoggedIn = props.isLoggedIn
+
+  if (isLoggedIn) {
+    return <UserGreeting /> 
+  }
+  return <GuestGreeting />
+}
+
+// button components
+
+const LoginButton = (props) => {
+  return (
+    <button onClick={props.onClick}>
+      Login
+    </button>
+  )
+}
+
+const LogoutButton = (props) => {
+  return (
+    <button onClick={props.onClick}>
+      Logout
+    </button>
+  )
+}
+
+
+// stateful class component
+// to capture button's click state
+
+class LoginControl extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      isLoggedIn: true
+    }
+
+    this.onLogin = this.onLogin.bind(this)
+    this.onLogout = this.onLogout.bind(this)
+  }
+
+  onLogin() {
+    console.log("you're logged in")
+    this.setState({
+      isLoggedIn: true
+    })
+  }
+
+  onLogout() {
+    console.log("you're logged out")
+    this.setState({
+      isLoggedIn: false
+    })
+  }
+
   render() {
+    //  destructuring component state assignment
+    const { isLoggedIn } = this.state
+    let button
+
+    // shortening constructor methods
+    // const login = this.onLogin()
+    // const logout = this.onLogout()
+
+    if (isLoggedIn) {
+      button = <LoginButton onClick={this.onLogin} />
+    } else {
+      button = <LogoutButton onClick={this.onLogout} />
+    }
+
     return (
       <div>
-        <Table isLoggedIn={false}/>
-        <LoginControl />
+        <Greeting isLoggedIn={isLoggedIn} />
+        {button}
       </div>
     )
   }
 }
 
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hey there</h1>
+      <LoginControl />
+    </div>
+  )
+}
+
+export default App
