a75bcb66ee7660e8dee43a266a9bf1e1a6d5c01b
Caleb335 <belac335@gmail.com>
2020-08-29 20:22:37 +0100

    synthetic event with hooks

--- src/components/App.js
+++ src/components/App.js
@@ -5,15 +5,15 @@ import { list } from '../assets/data'
 const App = () => {
   return (
     <div className="app__base">
-      <Search />
-    {list.map(item => {
-      return (
-        <div key={item.objectID}>
-          <h1>Title: {item.title}</h1>
-          <p>popularity: {item.points} </p>
-        </div>
-      )
-    })}
+      <Search id="search" name="search" />
+      {list.map((item) => {
+        return (
+          <div key={item.objectID}>
+            <h2>Title: {item.title}</h2>
+            <p>popularity: {item.points} </p>
+          </div>
+        )
+      })}
     </div>
   )
 }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Search = (props) => {
+  const [search, setSearch] = useState('')
+
+  const handleSearch = (e) => {
+    console.log(e)
+
+    setSearch(e.target.value)
+  }
+
   return (
     <div className="search__base">
-      <input type={props.type} id={props.id} name={props.name} />
+      <input
+        type={props.type}
+        id={props.id}
+        name={props.name}
+        onChange={handleSearch}
+        value={search}
+      />
     </div>
   )
 }
